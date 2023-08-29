b58d8c68a5454ad73d056ada27b76426e83b1bc4
nkct <miloszzegota@gmail.com>
2023-08-29 10:14:16 +0200

    added sidebar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,60 @@
 <script lang="ts">
 	import Captcha from "./Captcha.svelte";
+	import Sidebar from "./Sidebar.svelte";
 
     let captchas: {length: number, with_noise: boolean, with_bars: boolean}[] = [];
 </script>
 
-<h1>CAPTCHA</h1>
-<form on:submit={(e) => {
-    let target = e.target;
-    if (target != null && target instanceof HTMLFormElement) {
-        let form_data = Object.fromEntries(new FormData(target));
-        let data = {
-            length: Number(form_data.length), 
-            with_noise: false, 
-            with_bars: false
+<Sidebar />
+
+<div class="content">
+    <h1>CAPTCHA</h1>
+    <form class="text-captcha-form" on:submit={(e) => {
+        let target = e.target;
+        if (target != null && target instanceof HTMLFormElement) {
+            let form_data = Object.fromEntries(new FormData(target));
+            let data = {
+                length: Number(form_data.length), 
+                with_noise: false, 
+                with_bars: false
+            }
+            if (form_data.hasOwnProperty("with_noise")) {
+                data.with_noise = true
+            }
+            if (form_data.hasOwnProperty("with_bars")) {
+                data.with_bars = true
+            }
+    
+            captchas = [...captchas, data]
         }
-        if (form_data.hasOwnProperty("with_noise")) {
-            data.with_noise = true
-        }
-        if (form_data.hasOwnProperty("with_bars")) {
-            data.with_bars = true
-        }
+    }}>
+        <input type="number" name="length" value="5" style="width: 50px;"/>
+        <label for="length">Length</label>
+    
+        <input type="checkbox" name="with_noise" />
+        <label for="with_noise">With noise</label>
+    
+        <input type="checkbox" name="with_bars" />
+        <label for="with_bars">With bars</label>
 
-        captchas = [...captchas, data]
-    }
-}}>
-    <input type="number" name="length" value="5"/>
-    <label for="length">Length</label>
-
-    <input type="checkbox" name="with_noise" />
-    <label for="with_noise">With noise</label>
-
-    <input type="checkbox" name="with_bars" />
-    <label for="with_bars">With bars</label>
-
-    <input type="submit" value="Get CAPTCHA">
-</form>
-<div class="captcha_container">
-    {#each captchas as props}
-      <Captcha length = {props.length} with_noise = {props.with_noise} with_bars = {props.with_bars} />
-    {/each}
+        <br>
+    
+        <input type="submit" value="Get CAPTCHA">
+    </form>
+    <div class="captcha_container">
+        {#each captchas as props}
+          <Captcha length = {props.length} with_noise = {props.with_noise} with_bars = {props.with_bars} />
+        {/each}
+    </div>
 </div>
 
 <style lang="scss">
+    .content {
+        text-align: center;
+    }
+    .text-captcha-form {
+        display: block;
+    }
     .captcha_container {
         display: flex;
         justify-content: center;
---
+++ src/routes/Sidebar.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    let sidebar: HTMLDivElement;
+    let sidebar_close: HTMLButtonElement;
+
+    onMount(() => {
+        sidebar.style.width = "300px"
+        sidebar_close.style.transform = "rotate(180deg)"
+    })
+
+    function handleSidebar() {
+        sidebar_close.style.transform = "rotate(" + String(Number(sidebar_close.style.transform.slice(7, -4)) + 180) + "deg)"
+        if (Number(sidebar.style.width.slice(0, -2)) > 30) {
+            sidebar.style.width = "30px"
+        } else {
+            sidebar.style.width = "300px"
+        }
+    }
+</script>
+
+<div class="sidebar" bind:this={sidebar}>
+    <button class="sidebar-close" bind:this={sidebar_close} on:click={handleSidebar}> > </button>
+</div>
+
+<style lang="scss">
+    .sidebar {
+        background-color: rgb(228, 228, 228);
+        width: 300px;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        transition: width 0.5s;
+    }
+    .sidebar-close {
+        background-color: rgb(202, 202, 202);
+        padding: 10px;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: rotate(180deg);
+        transition: transform 0.5s;
+    }
+</style>
